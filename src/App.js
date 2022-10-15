ddfbd79721cb56e18e133c618c49c8ec9f388214
pouriya chibaie <pouriya.chibaie.dev@gmail.com>
2022-10-15 13:22:42 +0330

    add  socket connection to useCallback()

--- src/App.js
+++ src/App.js
@@ -1,36 +1,43 @@
 import './App.css';
-import{useEffect,useReducer,useState} from 'react'
+import { useCallback, useEffect, useReducer, useState } from 'react'
 import socketIO from "socket.io-client"
 function App() {
   const nameInitialState = ""
-const reducer = (state = nameInitialState, action) => {
+  const reducer = (state = nameInitialState, action) => {
     switch (action.type) {
       case "UPDATE_STATE":
         return action.data
-      
+
       default:
         return state
     }
   }
-  const [state,dispatch] = useReducer(reducer,"")
+  const [count, setCount] = useState(0)
+  const [socketBool, setSocketBool] = useState(false)
+  const [state, dispatch] = useReducer(reducer, "")
 
 
+  const socketConnection = useCallback(()=>{
+    const socket = socketIO("http://localhost:3010/socket")
+
+    socket.on("balance", (data) => {
+      dispatch({ type: "UPDATE_STATE", data })
+    })
+    return () => socket.on("disconnect", () => {
+      console.log("user disconnect client");
+    })
+
+  },[socketBool])
   useEffect(()=>{
- const socket=socketIO("http://localhost:3010/socket")
-
-   socket.on("balance",(data)=>{
-     dispatch({type:"UPDATE_STATE",data})
-   })  
-   return ()=>socket.on("disconnect",()=>{
-    console.log("user disconnect client");
-   })
-},[])
-console.log("new data",state);
+socketConnection()
+  },[])
+  console.log("new data", state);
 
   return (
     <div className="App">
-     <h1>{state.balance}</h1>
-     <h1>{state.tavankharid}</h1>
+      <h1>{state.balance}</h1>
+      <h1>{state.tavankharid}</h1>
+      <button onClick={()=>{setCount((prev)=>prev + 1)}}>{count}</button>
     </div>
   );
 }
