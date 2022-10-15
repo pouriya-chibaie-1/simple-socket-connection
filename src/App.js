2166fd3d24c070cbd484ab9b4a3551e217a8e6f8
pouriya chibaie <pouriya.chibaie.dev@gmail.com>
2022-10-15 12:55:57 +0330

    delete strict mode

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import './App.css';
-import{useEffect,useState} from 'react'
+import{useEffect,useReducer,useState} from 'react'
 import socketIO from "socket.io-client"
 function App() {
-  const [state,setState] = useState("")
+  const nameInitialState = ""
+const reducer = (state = nameInitialState, action) => {
+    switch (action.type) {
+      case "UPDATE_STATE":
+        return action.data
+      
+      default:
+        return state
+    }
+  }
+  const [state,dispatch] = useReducer(reducer,"")
+
+
   useEffect(()=>{
- socket=socketIO("http://localhost:3010/socket")
-socket.on("balance",(data)=>{
-  setState(data)
-})  
+ const socket=socketIO("http://localhost:3010/socket")
+
+   socket.on("balance",(data)=>{
+     dispatch({type:"UPDATE_STATE",data})
+   })  
+   return ()=>socket.on("disconnect",()=>{
+    console.log("user disconnect client");
+   })
 },[])
 console.log("new data",state);
+
   return (
     <div className="App">
      <h1>{state.balance}</h1>
